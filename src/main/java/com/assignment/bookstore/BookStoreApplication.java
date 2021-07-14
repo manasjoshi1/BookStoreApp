9add07851b4eb2f0e049bc6998a6c1b13c25b605
Manas Joshi <joshimanassunil@gmail.com>
2021-07-14 10:19:09 +0530

    "swagger 3 updated"

--- src/main/java/com/assignment/bookstore/BookStoreApplication.java
+++ src/main/java/com/assignment/bookstore/BookStoreApplication.java
@@ -2,8 +2,11 @@ package com.assignment.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
